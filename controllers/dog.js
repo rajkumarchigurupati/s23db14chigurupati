483f25b423a973736f1830e351f09cedd74bbf7e
rajkumarchigurupati <S567114@nwmissouri.edu>
2023-11-07 23:19:29 -0600

    Custom create on post

--- controllers/dog.js
+++ controllers/dog.js
@@ -35,10 +35,41 @@ res.send(`{"error": ${err}}`);
 exports.dog_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: dog detail: ' + req.params.id);
 };
+
+//Handle Custom create on Post
+exports.dog_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new dog();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"}
+    document.dog_color = req.body.dog_color;
+    document.dog_breed = req.body.dog_breed;
+    document.dog_price = req.body.dog_price;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
+
+
+
+
+/*
 // Handle Costume create on POST.
 exports.dog_create_post = function(req, res) {
 res.send('NOT IMPLEMENTED: dog create POST');
 };
+*/
+
+
+
+
 // Handle Costume delete form on DELETE.
 exports.dog_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: dog delete DELETE ' + req.params.id);
