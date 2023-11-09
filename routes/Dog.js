3da6a31502f0823c6934d79f428fa5e59a2e2aef
rajkumarchigurupati <S567114@nwmissouri.edu>
2023-11-08 22:22:47 -0600

    last upadate

--- routes/Dog.js
+++ routes/Dog.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/*
+/* GET home page. 
 router.get('/', function(req, res, next) {
   res.render('Dog', { title: 'Search Results Dog' });
 });
 
-
+*/
 
 const dog_controlers= require('../controllers/dog');
 /* GET costumes */
