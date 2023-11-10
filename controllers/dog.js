dead325da7d3ebf50731d21ca66429cd9828a04e
rajkumarchigurupati <S567114@nwmissouri.edu>
2023-11-10 03:43:21 -0600

    lab commit

--- controllers/dog.js
+++ controllers/dog.js
@@ -69,12 +69,7 @@ exports.dog_create_post = async function(req, res) {
 
 
 
-/*
-// Handle dog create on POST.
-exports.dog_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: dog create POST');
-};
-*/
+
 
 
 
@@ -83,7 +78,27 @@ res.send('NOT IMPLEMENTED: dog create POST');
 exports.dog_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: dog delete DELETE ' + req.params.id);
 };
-// Handle dog update form on PUT.
-exports.dog_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: dog update PUT' + req.params.id);
-};
+
+
+exports.dog_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body${JSON.stringify(req.body)}`)
+     try {
+    let toUpdate = await dog.findById( req.params.id)
+    // Do updates of properties
+    
+    if(req.body.dog_color)
+    toUpdate.dog_color = req.body.dog_color;
+    if(req.body.dog_breed) toUpdate.dog_breed = req.body.dog_breed;
+    if(req.body.dog_price) toUpdate.dog_price = req.body.dog_price;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+   failed`);
+    }
+   };
+
+
+
