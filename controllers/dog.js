94cd02074ea411633c538fe5179efd4b3021d582
rajkumarchigurupati <S567114@nwmissouri.edu>
2023-11-10 02:08:14 -0600

    LAbPart1

--- controllers/dog.js
+++ controllers/dog.js
@@ -1,5 +1,5 @@
 var dog = require('../models/dog');
-// List of all Costumes
+// List of all dogs
 exports.dog_list = async function(req, res) {
     try{
         thedogs = await dog.find();
@@ -28,13 +28,22 @@ res.send(`{"error": ${err}}`);
 
 
 
+// for a specific dog.
+exports.dog_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await dog.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+   };
 
 
 
-// for a specific Costume.
-exports.dog_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: dog detail: ' + req.params.id);
-};
+
+
 
 //Handle Custom create on Post
 exports.dog_create_post = async function(req, res) {
@@ -43,7 +52,7 @@ exports.dog_create_post = async function(req, res) {
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    // {"costume_type":"goat", "cost":12, "size":"large"}
+    // {"dog_type":"goat", "cost":12, "size":"large"}
     document.dog_color = req.body.dog_color;
     document.dog_breed = req.body.dog_breed;
     document.dog_price = req.body.dog_price;
@@ -61,7 +70,7 @@ exports.dog_create_post = async function(req, res) {
 
 
 /*
-// Handle Costume create on POST.
+// Handle dog create on POST.
 exports.dog_create_post = function(req, res) {
 res.send('NOT IMPLEMENTED: dog create POST');
 };
@@ -70,11 +79,11 @@ res.send('NOT IMPLEMENTED: dog create POST');
 
 
 
-// Handle Costume delete form on DELETE.
+// Handle dog delete form on DELETE.
 exports.dog_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: dog delete DELETE ' + req.params.id);
 };
-// Handle Costume update form on PUT.
+// Handle dog update form on PUT.
 exports.dog_update_put = function(req, res) {
 res.send('NOT IMPLEMENTED: dog update PUT' + req.params.id);
 };
