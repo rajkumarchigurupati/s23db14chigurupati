87eca8824d7ea5fbc93a1ebaf3c76eeb59648210
rajkumarchigurupati <S567114@nwmissouri.edu>
2023-11-14 14:16:12 -0600

    part 7 commit

--- controllers/dog.js
+++ controllers/dog.js
@@ -130,6 +130,21 @@ exports.dog_create_Page = function(req, res) {
     res.send(`{'error': '${err}'}`);
     }
    };
+   
+// Handle a delete one view with id from query
+exports.dog_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await dog.findById(req.query.id)
+    res.render('dogdelete', { title: 'dog Delete', toShow: 
+   result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
 
 
 
--- routes/Dog.js
+++ routes/Dog.js
@@ -22,5 +22,7 @@ router.get('/create', dog_controlers.dog_create_Page);
 /* GET create update page */
 router.get('/update', dog_controlers.dog_update_Page);
 
+/* GET delete dog page */
+router.get('/delete', dog_controlers.dog_delete_Page);
 
 module.exports = router;
